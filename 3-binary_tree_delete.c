e33093776e3ba0a5e3e80411c1c1fa498a4386fa
abrhamgg <abrhamgg@gmail.com>
2022-08-24 16:48:05 +0300

    delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binayr_tree_delete - fnnction that delete an entire binary tree
+ * @tree: pointer to the root node
+ * Return: void
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
